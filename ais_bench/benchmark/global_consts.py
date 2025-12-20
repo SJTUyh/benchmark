82c9a25f4c32fea8095814eb6e4dcd5e22916dd9
y30044005 <yanhe13@huawei.com>
2025-12-20 19:20:44 +0800

    client script

--- ais_bench/benchmark/global_consts.py
+++ ais_bench/benchmark/global_consts.py
@@ -5,4 +5,4 @@ REQUEST_TIME_OUT = None
 from logging import DEBUG, INFO, WARNING, ERROR, CRITICAL
 LOG_LEVEL = INFO # log level, choose from DEBUG, INFO, WARNING, ERROR, CRITICAL
 
-PENDING_TOKEN_RATE = 2
+PENDING_TOKEN_RATE = 2 # 设置越小，过程中降低Request Rate后响应越快，不建议小于1，同时越小可能导致无法及时发送足够请求
